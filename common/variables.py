18d0d13333c190b5900cb9bc038433babda528c8
Zhivlova <NataZhivlova@yandex.ru>
2022-02-12 20:41:31 +0300

    Безопасность

--- common/variables.py
+++ common/variables.py
@@ -15,7 +15,7 @@ ENCODING = 'utf-8'
 # Текущий уровень логирования
 LOGGING_LEVEL = logging.DEBUG
 # База данных для хранения данных сервера:
-SERVER_DATABASE = 'sqlite:///server_base.db3'
+SERVER_CONFIG = 'server.ini'
 
 # Прококол JIM основные ключи:
 ACTION = 'action'
@@ -24,6 +24,8 @@ USER = 'user'
 ACCOUNT_NAME = 'account_name'
 SENDER = 'from'
 DESTINATION = 'to'
+DATA = 'bin'
+PUBLIC_KEY = 'pubkey'
 
 # Прочие ключи, используемые в протоколе
 PRESENCE = 'presence'
@@ -37,17 +39,29 @@ LIST_INFO = 'data_list'
 REMOVE_CONTACT = 'remove'
 ADD_CONTACT = 'add'
 USERS_REQUEST = 'get_users'
+PUBLIC_KEY_REQUEST = 'pubkey_need'
 
 # Словари - ответы:
 # 200
 RESPONSE_200 = {RESPONSE: 200}
 # 202
 RESPONSE_202 = {RESPONSE: 202,
-                LIST_INFO:None
+                LIST_INFO: None
                 }
 # 400
 RESPONSE_400 = {
-            RESPONSE: 400,
-            ERROR: None
-        }
+    RESPONSE: 400,
+    ERROR: None
+}
+# 205
+RESPONSE_205 = {
+    RESPONSE: 205
+}
+
+# 511
+RESPONSE_511 = {
+    RESPONSE: 511,
+    DATA: None
+}
+
 
